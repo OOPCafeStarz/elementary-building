1dd8328371ed9f23bffb39f8ee6e59f7a00d713c
Seungchul Lee <lsc4719@gmail.com>
2016-05-03 18:13:55 +0900

    UserBoard class 수정 - String[][] 스트링 이차원 배열로 board 구현 - move(fromR, fromC, toR, toC) : 박스 이동 메소드 구현 - set(r, c, String item) : (r, c)에 item 놓는 메소드 구현 - clearAll() : 전체 " "로 채우기.

--- ElementaryBuilding/src/prototype/Constants.java
+++ ElementaryBuilding/src/prototype/Constants.java
@@ -3,4 +3,6 @@ package prototype;
 public class Constants {
 	public static final int USERBOARD_WIDTH = 4;
 	public static final int USERBOARD_HEIGHT = 8;
+	public static final int INIT_GRAB_POS_X = 0;
+	public static final int INIT_GRAB_POS_Y = 0;
 }
--- ElementaryBuilding/src/prototype/UserBoard.java
+++ ElementaryBuilding/src/prototype/UserBoard.java
@@ -1,5 +1,60 @@
 package prototype;
 
 public class UserBoard {
-	private char[][] board = new char[Constants.USERBOARD_WIDTH][Constants.USERBOARD_HEIGHT];
+	private String[][] board = new String[Constants.USERBOARD_WIDTH][Constants.USERBOARD_HEIGHT];
+	private static final int width = Constants.USERBOARD_WIDTH;
+	private static final int height = Constants.USERBOARD_HEIGHT;
+	int currentGrabPositionX = Constants.INIT_GRAB_POS_X;
+	int currentGrabPositionY = Constants.INIT_GRAB_POS_Y;
+	
+	public String[][] getBoard() {
+		return board;
+	}
+
+	public void setBoard(String[][] board) {
+		this.board = board;
+	}
+
+	public UserBoard(){}
+	
+	public void clearAll(){
+		for(int i = 0;i<Constants.USERBOARD_HEIGHT;i++){
+			for(int j=0;j<Constants.USERBOARD_WIDTH;j++){
+				board[i][j] = " ";
+			}
+		}
+	}
+	
+	public void move(int fromR, int fromC, int toR, int toC){
+		if(!inRange(fromR, fromC) || !inRange(toR, toC)){
+			System.out.println("ERROR[move() in UserBoard]: Can't move item in UserBorad");
+			return;
+		}
+		
+		board[toR][toC] = board[fromR][fromC];
+	}
+	
+	public void setItem(int r, int c, String item){
+		if(!inRange(r, c)){
+			System.out.println("ERROR[set() in UserBoard] : board OutOfBoundIndex error");
+			return;
+		}
+		
+		board[r][c] = item;
+	}
+	
+	public String getItem(int r, int c){
+		if(!inRange(r, c)){
+			System.out.println("ERROR[set() in UserBoard] : board OutOfBoundIndex error");
+			return " ";
+		}
+		return board[r][c];
+	}
+	
+	private boolean inRange(int r, int c){
+		if(r < 0 || r >= Constants.USERBOARD_HEIGHT
+				|| c < 0 || c >= Constants.USERBOARD_WIDTH)
+			return false;
+		return true;
+	}
 }
