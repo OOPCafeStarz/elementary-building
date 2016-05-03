292494525db657f7edb9cfed543929a4ee04f082
Seungchul Lee <lsc4719@gmail.com>
2016-05-03 17:11:51 +0900

    Constants 클래스 만들었음. - 상수들 저장하는 클래스. - 모든 상수는 public static final ... 이 될 듯?? UserBoard 클래스 만들었음. - 유저가 게임할 때 건물을 쌓는 그 보드임. - Play 클래스가 건물을 쌓을 수 있도록 여러 메소드들을 추가할 계획임.

---
+++ ElementaryBuilding/src/prototype/Constants.java
@@ -0,0 +1,6 @@
+package prototype;
+
+public class Constants {
+	public static final int USERBOARD_WIDTH = 4;
+	public static final int USERBOARD_HEIGHT = 8;
+}
---
+++ ElementaryBuilding/src/prototype/UserBoard.java
@@ -0,0 +1,5 @@
+package prototype;
+
+public class UserBoard {
+	private char[][] board = new char[Constants.USERBOARD_WIDTH][Constants.USERBOARD_HEIGHT];
+}
