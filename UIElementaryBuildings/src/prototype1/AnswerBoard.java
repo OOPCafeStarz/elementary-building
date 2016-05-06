d1afaac480b87f28b7bb8c670812098f904eeeef
Seungchul Lee <lsc4719@gmail.com>
2016-05-06 14:03:58 +0900

    AnswerBoard class 추가. - board는 String[][]으로 저장. - board의 getter만듦.

---
+++ UIElementaryBuildings/src/prototype1/AnswerBoard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype1;
+
+/**
+ *
+ * @author user
+ */
+public class AnswerBoard {
+    public static final int ANSWER_BOARD_ROW = 8;
+    public static final int ANSWER_BOARD_COL = 4;
+    
+    private String[][] board = new String[ANSWER_BOARD_ROW][ANSWER_BOARD_COL];
+
+    public String[][] getBoard() {
+        return board;
+    }
+    
+}
