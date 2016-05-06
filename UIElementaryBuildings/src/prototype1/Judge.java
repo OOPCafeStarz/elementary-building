f9ee3cf7fd0c2aa1317ed88327481d19c51497d0
kmg8280 <kmg8280@gmail.com>
2016-05-06 14:13:29 +0900

    Judge Method 구현

--- UIElementaryBuildings/src/prototype1/Judge.java
+++ UIElementaryBuildings/src/prototype1/Judge.java
@@ -2,7 +2,17 @@ package prototype1;
 
 public class Judge {
     public Judge() {}
-    public boolean mark(){
-        return false;
+    public boolean mark(UserBoard ub, AnswerBoard ab){
+        String[][] inputUserBoard = ub.getBoard();
+        String[][] inputAnswerBoard = ab.getBoard();
+        for(int i=0; i<UserBoard.USER_BOARD_ROW; i++){
+            for(int j=0; j<UserBoard.USER_BOARD_COL; j++){
+                if(inputUserBoard[i][j].equals(inputAnswerBoard[i][j])){
+                    continue;
+                }else
+                    return false;
+            }
+        }
+        return true;
     }
 }
