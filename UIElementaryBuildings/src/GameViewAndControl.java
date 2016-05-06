bb899af1f7bdadcc2a1af0d394645b175723a008
kmg8280 <kmg8280@gmail.com>
2016-05-06 13:46:47 +0900

    UIComponents 이름을 바꿧고 Judge 클래스 생성

--- UIElementaryBuildings/src/GameViewAndControl.java
+++ UIElementaryBuildings/src/GameViewAndControl.java
@@ -8,7 +8,7 @@
  *
  * @author user
  */
-public class UIComponents extends javax.swing.JDialog {
+public class GameViewAndControl extends javax.swing.JDialog {
 
     /**
      * Creates new form UIComponents
@@ -16,10 +16,9 @@ public class UIComponents extends javax.swing.JDialog {
     private int cnt = 0;
 	private char[] q = new char[]{' ',' ',' ',' ',' ',' ',' ',' '};
    
-    public UIComponents(java.awt.Frame parent, boolean modal) {
+    public GameViewAndControl(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        
         myInitComponents();
     }
     
@@ -44,7 +43,6 @@ public class UIComponents extends javax.swing.JDialog {
         
         uiUserBoard = jTable1;
         uiAnswerBoard = jTable2;
-        
         uiInstructionButtons[0].setText("■");
         uiInstructionButtons[1].setText("↓");
         for(int i=0;i<8;i++) uiInstructionQueue[i].setText(" ");
@@ -315,12 +313,14 @@ public class UIComponents extends javax.swing.JDialog {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+         
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
+            
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
@@ -328,21 +328,23 @@ public class UIComponents extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(UIComponents.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GameViewAndControl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UIComponents.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GameViewAndControl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(UIComponents.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GameViewAndControl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(UIComponents.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GameViewAndControl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                UIComponents dialog = new UIComponents(new javax.swing.JFrame(), true);
+                GameViewAndControl dialog = new GameViewAndControl(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
---
+++ UIElementaryBuildings/src/Judge.java
@@ -0,0 +1,6 @@
+public class Judge {
+    public Judge() {}
+    public boolean mark(UserBoard ub, AnswerBoard ab){
+        return false;
+    }
+}
