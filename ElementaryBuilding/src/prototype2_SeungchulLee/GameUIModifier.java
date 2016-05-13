b05615292759ab08f5908cf721077a8679b38c54
Seungchul Lee <lsc4719@gmail.com>
2016-05-13 19:45:31 +0900

    update

---
+++ ElementaryBuilding/src/prototype2_SeungchulLee/GameUIModifier.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototype2_SeungchulLee;
+
+/**
+ *
+ * @author user
+ */
+public class GameUIModifier {
+    public static void initAllGameUIConponents(){
+        // init user board
+        
+        // init answer board
+        
+        // init queue of current module
+    }
+    
+    public static void setIconInUserBoard(int x, int y, String icon){
+        
+    }
+    
+    public static void setIconInUserBoard(String[][] icons){
+        
+    }
+    
+    public static void setIconInAnswerBoard(String[][] icons){
+        
+    }
+    
+    public static void setIconInQueue(int queueIndex, String icon){
+        
+    }
+    
+    public static void setIconInqueue(String[] icons){
+        
+    }
+    
+    public static void setTextInJudge(String judegText){
+        
+    }
+}
